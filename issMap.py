0354d13f81edc2875b6c0865444c3d095b6beec0
shubham kumar <41818316+imshubhamkj@users.noreply.github.com>
2019-03-24 09:41:24 +0530

    Update issMap.py

--- issMap.py
+++ issMap.py
@@ -28,7 +28,7 @@ def getposition():
     return float(data['iss_position']['latitude']),float(data['iss_position']['longitude']),float(data['timestamp'])
 
 
-def issPositionOnMap(lat, lon):
+def issPositionOnMap(s,lat, lon, i):
 
     m = Basemap(projection='robin',lon_0=-105,lat_0=40,resolution='l')
     m.drawcoastlines()
@@ -37,17 +37,32 @@ def issPositionOnMap(lat, lon):
     
     #ploting astronauts location on base map.
     x, y = m(lon, lat)
-    m.plot(x, y, color='red', marker='s') 
+    m.plot(x, y, color='red', marker='o')
+    x2, y2 = m(lon+10,lat+10)  
+    plt.annotate('iss', xy=(x, y),  xycoords='data',
+                xytext=(x2, y2), textcoords='data',bbox=dict(boxstyle="round", alpha=0.8),
+                arrowprops=dict(arrowstyle="fancy", color='b')
+                )
     plt.title('International Space Station Positon')
-    plt.show()
+    plt.xlabel('longitude:'+str(lon)+'  '+'latitude:'+str(lat))
+    
+    #address = 'z'+str(i)+'.png'
+    
+    #plt.savefig(address)
+    plt.show(block=False)
+    plt.pause(5)
+    plt.close()
+    
 
 
 if __name__ == "__main__":
     print("List of Astronauts on space station\n",getastronauts())
     flag=True
+    i=0
     while(flag):
         lat, lon,timestamp = getposition()
         print("\n")
         print("------map for timestamp",timestamp,"On the server------\n\n","position of International space station now \n ","latitude:",lat,"longitude:",lon)
-        issPositionOnMap(lat, lon)
+        issPositionOnMap(getastronauts(),lat, lon, i)
+        i=i+1
     
